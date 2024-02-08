10e92700e7086718b5dd7364128aaa9d7b20baaa
preston <polston13@outlook.com>
2024-02-08 17:02:42 -0500

    need to fix updateProduct

--- db/product.js
+++ db/product.js
@@ -21,4 +21,60 @@ const getProductById = async (id) => {
   }
 };
 
-module.exports = { getAllProducts, getProductById };
+const createNewProduct = async (req) => {
+  console.log(req.body);
+  try {
+    return await prisma.product.create({
+      data: {
+        name: req.body.name,
+        price: req.body.price,
+        image: req.body.image,
+        description: req.body.description,
+        class: req.body.class,
+      },
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+const updateProduct = async (id, req) => {
+  try {
+    const updateProduct = await prisma.product.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        name: req.body.name,
+        price: req.body.price,
+        image: req.body.image,
+        description: req.body.description,
+        class: req.body.class,
+      },
+    });
+    return updateProduct;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const deleteProduct = async (req) => {
+  try {
+    const Product = await prisma.product.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    return;
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+  createNewProduct,
+  updateProduct,
+  deleteProduct,
+};
