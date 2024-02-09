b75964a59e07b0be320c3f44fd936b5e23a92f33
bbizz <bbizz@DESKTOP-F1BIQ76>
2024-02-09 14:45:52 -0500

    middleware

--- auth/midleware.js
+++ auth/midleware.js
@@ -11,10 +11,10 @@ const isLoggedIn = async (req, res, next) => {
 };
 
 const isAdmin = (req, res, next) => {
-  if (req.user.is_admin) {
+  if (req.user && req.user.is_admin) {
     next();
   } else {
-    const error = Error("must be admin");
+    const error = Error("Unauthorized: Admin access required");
     error.status = 401;
     next(error);
   }
