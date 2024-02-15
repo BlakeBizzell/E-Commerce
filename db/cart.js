d486d1c2c81b002c8babb0d7e27c44e15249bb03
preston <polston13@outlook.com>
2024-02-15 12:43:27 -0500

    finished end points

--- db/cart.js
+++ db/cart.js
@@ -1,6 +1,16 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+// get cart
+const getCart = async (userId) => {
+  const cartItems = await prisma.cart.findMany({
+    where: { userId: parseInt(userId) },
+    include: { product: true },
+  });
+  return cartItems;
+};
+
+// add to cart
 const addToCart = async (userId, productId, quantity) => {
   try {
     const user = await prisma.users.findUnique({
@@ -34,4 +44,32 @@ const addToCart = async (userId, productId, quantity) => {
   }
 };
 
-module.exports = { addToCart };
+// remove from cart
+const removeFromCart = async (userId, cartItemId) => {
+  try {
+    const cartItem = await prisma.cart.findUnique({
+      where: {
+        id: parseInt(cartItemId),
+        userId: parseInt(userId),
+      },
+    });
+
+    if (!cartItem) {
+      return { success: false, error: "Cart item not found" };
+    }
+
+    await prisma.cart.delete({
+      where: { id: parseInt(cartItemId) },
+    });
+
+    return { success: true };
+  } catch (error) {
+    console.error("Error removing item from cart:", error);
+    return {
+      success: false,
+      error: "An error occurred while removing item from cart",
+    };
+  }
+};
+
+module.exports = { getCart, addToCart, removeFromCart };
